91401172101d7dc6e741a1191eebe8570ecd8961
sunhaikuo <haikuo.sun@mtime.com>
2018-01-09 20:10:04 +0800

    删除

--- index.js
+++
@@ -1,8 +0,0 @@
-// let path = require('path')
-let Index = require('./lib/Index')
-// let index = new Index({
-//     path: path.resolve(__dirname, 'src/User.ts'),
-//     output: path.resolve(__dirname, './types/util.d.ts')
-// })
-// index.run()
-export = Index
